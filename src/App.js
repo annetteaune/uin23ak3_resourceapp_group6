1ba82870ef50d7f3565dd8f0e32ce1a687b53720
annetteaune <annetteaune@outlook.com>
2023-02-21 22:55:52 +0100

    states for sortering og aktiv tab lagt inn

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import "./css/main.css";
 import Layout from "./components/Layout";
 import resources from "./ressurser";
+import { useState } from "react";
 
 function App() {
 	// Mappe ut kun unike kategorinavn, kilde:
 	// https://plainenglish.io/blog/how-to-get-distinct-values-from-an-array-of-objects-in-javascript
 	const categoryList = [...new Set(resources.map((cat) => cat.category))];
-  
+
+	// State for aktiv tab, default på html
+	let [selectedTab, setSelectedTab] = useState("html");
+
+	// State for å bytte farge på aktiv tab
+	// Kilde: https://stackoverflow.com/questions/56114153/add-active-class-on-a-map-items-react
+	const [tabIndex, setTabIndex] = useState(0);
+
+	const filter = resources.filter((find) => find.category === selectedTab);
+
 	return (
 		<div className="App">
-			<Layout categoryList={categoryList} resources={resources}/>
+			<Layout
+				categoryList={categoryList}
+				resources={resources}
+				selectedTab={selectedTab}
+				setSelectedTab={setSelectedTab}
+				filter={filter}
+        tabIndex={tabIndex}
+        setTabIndex={setTabIndex}
+			/>
 		</div>
 	);
 }
