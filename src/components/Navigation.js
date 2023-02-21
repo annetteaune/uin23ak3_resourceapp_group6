dd6cfb1750278c0ab1d2e5b650e8e840ddc3cfe0
annetteaune <annetteaune@outlook.com>
2023-02-21 21:20:59 +0100

    Lagt til sass-kompilering, lagt inn sass/css

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,12 @@ export default function Navigation({ categoryList }) {
 	return (
 		<ul>
 			{categoryList.map((cat, index) => {
-				return <li> {cat}</li>;
+				return (
+					<li key={index}>
+						{" "}
+						<a href={"/"}> {cat} </a>{" "}
+					</li>
+				);
 			})}
 		</ul>
 	);
